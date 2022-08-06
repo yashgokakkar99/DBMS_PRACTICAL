7622450ca558e04a00e1919674fde9b4c7f7370e
Ashutosh Raj Gupta <89141709+AshutoshRajGupta@users.noreply.github.com>
2022-08-07 04:32:34 +0530

    Update view.sql

--- view.sql
+++ view.sql
@@ -1,1 +1,15 @@
 
+/* Views in SQL are kind of virtual tables. A view also has rows and columns as they are in a real table in the database. 
+We can create a view by selecting fields from one or more tables present in the database. A View can either have all the rows 
+of a table or specific rows based on certain condition.*/
+
+-- creating a view 
+
+
+-- inserting into view
+
+
+-- updating into view
+
+-- deleting into a view
+
