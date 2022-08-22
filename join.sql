844f4a733f1d490dff41ae2d9e28cc6a644d9138
Ashutosh Raj Gupta <89141709+AshutoshRajGupta@users.noreply.github.com>
2022-08-22 13:52:47 +0530

    Update join.sql

--- join.sql
+++ join.sql
@@ -1,3 +1,54 @@
+CREATE TABLE `users` (
+  `auid` int(10) UNSIGNED NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `createdate` datetime NOT NULL,
+  `isActive` tinyint(1) NOT NULL
+);
+
+
+CREATE TABLE `userprofile` (
+  `apid` int(10) UNSIGNED NOT NULL,
+  `auid` int(10) UNSIGNED NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(45) NOT NULL
+);
+
+Insert into users
+(auid, username,password, createdate, isActive)
+values
+(1,'admin','pswrd123', curdate(), 1);
+Insert into userprofile
+(apid, auid, firstname, lastname, email, phone)
+values
+(1,1,'Jack', 'Wolf', 'bettestroom@gmail.com','600075764216');
+Insert into users
+(auid,username,password, createdate, isActive)
+values
+(2, 'admin1','pass506', curdate(), 1);
+Insert into userprofile
+(apid, auid, firstname, lastname, email, phone)
+values
+(2, 3, 'Tom', 'Collins', 'tnkc@outlook.com','878511311054');
+Insert into users
+(auid, username,password, createdate, isActive)
+values
+(4,'fox12','45@jgo0', curdate(), 1);
+Insert into userprofile
+(apid, auid, firstname, lastname, email, phone)
+values
+(4,5,'Bill', 'Fonskin', 'bill_1290@gmail.com','450985764216');
+Insert into users
+(auid,username,password, createdate, isActive)
+values
+(6, 'lexus1267','98hnfRT6', curdate(), 1);
+Insert into userprofile
+(apid, auid, firstname, lastname, email, phone)
+values
+(7, 7, 'Ivan', 'Levchenko', 'ivan_new@outlook.com','878511311054');
+
 mysql> select * from users;
 +------+-----------+----------+---------------------+----------+
 | auid | username  | password | createdate          | isActive |
