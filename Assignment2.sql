1fe16f38a7369db2c535a7dbc600914579133cc4
Ashutosh Raj Gupta <89141709+AshutoshRajGupta@users.noreply.github.com>
2022-08-23 16:31:58 +0530

    Update Assignment2.sql

--- Assignment2.sql
+++ Assignment2.sql
@@ -1,74 +1,165 @@
---INDEX
-mysql> select * from Book;
-+---------+-------+--------+-------+
-| book_id | bname | Author | price |
-+---------+-------+--------+-------+
-|       1 | DBMS  | XYZ    |   400 |
-|       2 | OS    | ABC    |   250 |
-|       3 | SP    | MNO    |   300 |
-|       4 | CNS   | PQR    |   200 |
-|       5 | TOC   | QWE    |   450 |
-|       6 | SPM   | ARE    |   500 |
-+---------+-------+--------+-------+
-6 rows in set (0.00 sec)
-
---creating index
-mysql> create index index_bname on Book(bname);
-Query OK, 0 rows affected (0.05 sec)
-Records: 0  Duplicates: 0  Warnings: 0
-
- --droping index
-mysql> drop index index_bname on Book;
-Query OK, 0 rows affected (0.02 sec)
-Records: 0  Duplicates: 0  Warnings: 0
-
-mysql> create index index_bname on Book(bname);
-Query OK, 0 rows affected (0.03 sec)
-Records: 0  Duplicates: 0  Warnings: 0
-
---Alter command in index
-
-mysql> alter table Book add index index_price (price);
-Query OK, 0 rows affected (0.04 sec)
-Records: 0  Duplicates: 0  Warnings: 0
-
-mysql> Alter table Book modify book_id int primary key;
-Query OK, 0 rows affected (0.10 sec)
-Records: 0  Duplicates: 0  Warnings: 0
-
-mysql> Alter table Book drop primary key;
-Query OK, 6 rows affected (0.08 sec)
-Records: 6  Duplicates: 0  Warnings: 0
+CREATE TABLE `users` (
+  `auid` int(10) UNSIGNED NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `createdate` datetime NOT NULL,
+  `isActive` tinyint(1) NOT NULL
+);
 
 
-mysql> alter table Book add index(book_id);
-Query OK, 0 rows affected (0.03 sec)
-Records: 0  Duplicates: 0  Warnings: 0
+CREATE TABLE `userprofile` (
+  `apid` int(10) UNSIGNED NOT NULL,
+  `auid` int(10) UNSIGNED NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(45) NOT NULL
+);
 
+Insert into users
+(auid, username,password, createdate, isActive)
+values
+(1,'admin','pswrd123', curdate(), 1);
+Insert into userprofile
+(apid, auid, firstname, lastname, email, phone)
+values
+(1,1,'Jack', 'Wolf', 'bettestroom@gmail.com','600075764216');
+Insert into users
+(auid,username,password, createdate, isActive)
+values
+(2, 'admin1','pass506', curdate(), 1);
+Insert into userprofile
+(apid, auid, firstname, lastname, email, phone)
+values
+(2, 3, 'Tom', 'Collins', 'tnkc@outlook.com','878511311054');
+Insert into users
+(auid, username,password, createdate, isActive)
+values
+(4,'fox12','45@jgo0', curdate(), 1);
+Insert into userprofile
+(apid, auid, firstname, lastname, email, phone)
+values
+(4,5,'Bill', 'Fonskin', 'bill_1290@gmail.com','450985764216');
+Insert into users
+(auid,username,password, createdate, isActive)
+values
+(6, 'lexus1267','98hnfRT6', curdate(), 1);
+Insert into userprofile
+(apid, auid, firstname, lastname, email, phone)
+values
+(7, 7, 'Ivan', 'Levchenko', 'ivan_new@outlook.com','878511311054');
 
-mysql> /* drop a index*/
-mysql> alter table Book drop index index_price;
-Query OK, 0 rows affected (0.02 sec)
-Records: 0  Duplicates: 0  Warnings: 0
+mysql> select * from users;
++------+-----------+----------+---------------------+----------+
+| auid | username  | password | createdate          | isActive |
++------+-----------+----------+---------------------+----------+
+|    1 | admin     | pswrd123 | 2022-08-13 00:00:00 |        1 |
+|    2 | admin1    | pass506  | 2022-08-13 00:00:00 |        1 |
+|    4 | fox12     | 45@jgo0  | 2022-08-13 00:00:00 |        1 |
+|    6 | lexus1267 | 98hnfRT6 | 2022-08-13 00:00:00 |        1 |
++------+-----------+----------+---------------------+----------+
+4 rows in set (0.00 sec)
 
---showing indexes
-mysql> show index from Book;
-+-------+------------+-------------+--------------+-------------+-----------+-------------+----------+--------+------+------------+---------+---------------+---------+------------+
-| Table | Non_unique | Key_name    | Seq_in_index | Column_name | Collation | Cardinality | Sub_part | Packed | Null | Index_type | Comment | Index_comment | Visible | Expression |
-+-------+------------+-------------+--------------+-------------+-----------+-------------+----------+--------+------+------------+---------+---------------+---------+------------+
-| book  |          1 | index_bname |            1 | bname       | A         |           6 |     NULL |   NULL | YES  | BTREE      |         |               | YES     | NULL       |
-| book  |          1 | book_id     |            1 | book_id     | A         |           6 |     NULL |   NULL |      | BTREE      |         |               | YES     | NULL       |
-+-------+------------+-------------+--------------+-------------+-----------+-------------+----------+--------+------+------------+---------+---------------+---------+------------+
-2 rows in set (0.02 sec)
+mysql> select * from userprofile;
++------+------+-----------+-----------+-----------------------+--------------+
+| apid | auid | firstname | lastname  | email                 | phone        |
++------+------+-----------+-----------+-----------------------+--------------+
+|    1 |    1 | Jack      | Wolf      | bettestroom@gmail.com | 600075764216 |
+|    2 |    3 | Tom       | Collins   | tnkc@outlook.com      | 878511311054 |
+|    4 |    5 | Bill      | Fonskin   | bill_1290@gmail.com   | 450985764216 |
+|    7 |    7 | Ivan      | Levchenko | ivan_new@outlook.com  | 878511311054 |
++------+------+-----------+-----------+-----------------------+--------------+
+4 rows in set (0.00 sec)
 
-mysql> drop index index_bname on Book;
-Query OK, 0 rows affected (0.02 sec)
-Records: 0  Duplicates: 0  Warnings: 0
-
-mysql> show index from Book;
-+-------+------------+----------+--------------+-------------+-----------+-------------+----------+--------+------+------------+---------+---------------+---------+------------+
-| Table | Non_unique | Key_name | Seq_in_index | Column_name | Collation | Cardinality | Sub_part | Packed | Null | Index_type | Comment | Index_comment | Visible | Expression |
-+-------+------------+----------+--------------+-------------+-----------+-------------+----------+--------+------+------------+---------+---------------+---------+------------+
-| book  |          1 | book_id  |            1 | book_id     | A         |           6 |     NULL |   NULL |      | BTREE      |         |               | YES     | NULL       |
-+-------+------------+----------+--------------+-------------+-----------+-------------+----------+--------+------+------------+---------+---------------+---------+------------+
+/*synatx-SELECT columns FROM tableA 
+INNER JOIN tableB
+ON tableA.column = tableB.column;
+*/
+mysql> select t2.apid,t1.auid,t1.username,t2.firstname,t2.email,t1.password from users as t1 inner join
+    -> userprofile as t2 where t1.auid=t2.auid;
++------+------+----------+-----------+-----------------------+----------+
+| apid | auid | username | firstname | email                 | password |
++------+------+----------+-----------+-----------------------+----------+
+|    1 |    1 | admin    | Jack      | bettestroom@gmail.com | pswrd123 |
++------+------+----------+-----------+-----------------------+----------+
 1 row in set (0.00 sec)
+
+mysql> select * from users inner join userprofile where users.auid=userprofile.auid;
++------+----------+----------+---------------------+----------+------+------+-----------+----------+-----------------------+--------------+
+| auid | username | password | createdate          | isActive | apid | auid | firstname | lastname | email                 | phone        |
++------+----------+----------+---------------------+----------+------+------+-----------+----------+-----------------------+--------------+
+|    1 | admin    | pswrd123 | 2022-08-13 00:00:00 |        1 |    1 |    1 | Jack      | Wolf     | bettestroom@gmail.com | 600075764216 |
++------+----------+----------+---------------------+----------+------+------+-----------+----------+-----------------------+--------------+
+1 row in set (0.00 sec)
+
+--cross join
+/*syntax-SELECT columns FROM tableA
+CROSS JOIN tableB;
+*/
+mysql> select * from users cross join userprofile ;
++------+-----------+----------+---------------------+----------+------+------+-----------+-----------+-----------------------+--------------+
+| auid | username  | password | createdate          | isActive | apid | auid | firstname | lastname  | email                 | phone        |
++------+-----------+----------+---------------------+----------+------+------+-----------+-----------+-----------------------+--------------+
+|    6 | lexus1267 | 98hnfRT6 | 2022-08-13 00:00:00 |        1 |    1 |    1 | Jack      | Wolf      | bettestroom@gmail.com | 600075764216 |
+|    4 | fox12     | 45@jgo0  | 2022-08-13 00:00:00 |        1 |    1 |    1 | Jack      | Wolf      | bettestroom@gmail.com | 600075764216 |
+|    2 | admin1    | pass506  | 2022-08-13 00:00:00 |        1 |    1 |    1 | Jack      | Wolf      | bettestroom@gmail.com | 600075764216 |
+|    1 | admin     | pswrd123 | 2022-08-13 00:00:00 |        1 |    1 |    1 | Jack      | Wolf      | bettestroom@gmail.com | 600075764216 |
+|    6 | lexus1267 | 98hnfRT6 | 2022-08-13 00:00:00 |        1 |    2 |    3 | Tom       | Collins   | tnkc@outlook.com      | 878511311054 |
+|    4 | fox12     | 45@jgo0  | 2022-08-13 00:00:00 |        1 |    2 |    3 | Tom       | Collins   | tnkc@outlook.com      | 878511311054 |
+|    2 | admin1    | pass506  | 2022-08-13 00:00:00 |        1 |    2 |    3 | Tom       | Collins   | tnkc@outlook.com      | 878511311054 |
+|    1 | admin     | pswrd123 | 2022-08-13 00:00:00 |        1 |    2 |    3 | Tom       | Collins   | tnkc@outlook.com      | 878511311054 |
+|    6 | lexus1267 | 98hnfRT6 | 2022-08-13 00:00:00 |        1 |    4 |    5 | Bill      | Fonskin   | bill_1290@gmail.com   | 450985764216 |
+|    4 | fox12     | 45@jgo0  | 2022-08-13 00:00:00 |        1 |    4 |    5 | Bill      | Fonskin   | bill_1290@gmail.com   | 450985764216 |
+|    2 | admin1    | pass506  | 2022-08-13 00:00:00 |        1 |    4 |    5 | Bill      | Fonskin   | bill_1290@gmail.com   | 450985764216 |
+|    1 | admin     | pswrd123 | 2022-08-13 00:00:00 |        1 |    4 |    5 | Bill      | Fonskin   | bill_1290@gmail.com   | 450985764216 |
+|    6 | lexus1267 | 98hnfRT6 | 2022-08-13 00:00:00 |        1 |    7 |    7 | Ivan      | Levchenko | ivan_new@outlook.com  | 878511311054 |
+|    4 | fox12     | 45@jgo0  | 2022-08-13 00:00:00 |        1 |    7 |    7 | Ivan      | Levchenko | ivan_new@outlook.com  | 878511311054 |
+|    2 | admin1    | pass506  | 2022-08-13 00:00:00 |        1 |    7 |    7 | Ivan      | Levchenko | ivan_new@outlook.com  | 878511311054 |
+|    1 | admin     | pswrd123 | 2022-08-13 00:00:00 |        1 |    7 |    7 | Ivan      | Levchenko | ivan_new@outlook.com  | 878511311054 |
++------+-----------+----------+---------------------+----------+------+------+-----------+-----------+-----------------------+--------------+
+16 rows in set (0.00 sec)
+
+--left join
+/*
+SELECT columns FROM tableA
+LEFT [OUTER] JOIN tableB
+ON tableA.column = tableB.column;
+*/
+mysql> select t1.auid,t1.username,t1.password,t2.firstname,t2.lastname from users as t1 left join userprofile as t2 on  t1.auid=t2.auid;
++------+-----------+----------+-----------+----------+
+| auid | username  | password | firstname | lastname |
++------+-----------+----------+-----------+----------+
+|    1 | admin     | pswrd123 | Jack      | Wolf     |
+|    2 | admin1    | pass506  | NULL      | NULL     |
+|    4 | fox12     | 45@jgo0  | NULL      | NULL     |
+|    6 | lexus1267 | 98hnfRT6 | NULL      | NULL     |
++------+-----------+----------+-----------+----------+
+4 rows in set (0.00 sec)
+
+--right join
+/* 
+SELECT columns FROM tableA 
+RIGHT [OUTER] JOIN tableB
+ON tableA.column = tableB.column;
+*/
+mysql> select t1.auid,t1.username,t1.password,t2.firstname,t2.lastname from users as t1 right join userprofile as t2 on  t1.auid=t2.auid;
++------+----------+----------+-----------+-----------+
+| auid | username | password | firstname | lastname  |
++------+----------+----------+-----------+-----------+
+|    1 | admin    | pswrd123 | Jack      | Wolf      |
+| NULL | NULL     | NULL     | Tom       | Collins   |
+| NULL | NULL     | NULL     | Bill      | Fonskin   |
+| NULL | NULL     | NULL     | Ivan      | Levchenko |
++------+----------+----------+-----------+-----------+
+4 rows in set (0.00 sec)
+--using
+mysql> select t1.auid,t1.username,t1.password,t2.firstname,t2.lastname from users as t1 right join userprofile as t2 using(auid);
++------+----------+----------+-----------+-----------+
+| auid | username | password | firstname | lastname  |
++------+----------+----------+-----------+-----------+
+|    1 | admin    | pswrd123 | Jack      | Wolf      |
+| NULL | NULL     | NULL     | Tom       | Collins   |
+| NULL | NULL     | NULL     | Bill      | Fonskin   |
+| NULL | NULL     | NULL     | Ivan      | Levchenko |
++------+----------+----------+-----------+-----------+
+4 rows in set (0.00 sec)
