b31b76a94452e8ab700446f8d696c7563bc473be
Ashutosh Raj Gupta <89141709+AshutoshRajGupta@users.noreply.github.com>
2022-09-12 14:18:19 +0530

    Update plsql_assignment_4.sql

--- plsql_assignment_4.sql
+++ plsql_assignment_4.sql
@@ -7,29 +7,9 @@ Table created.
 SQL> declare
   2      radius_var number;
   3      area_var number;
-  4  begin
-  5      for radius_var in 5..8 loop
-  6          area_var:=pi*radius_var*radius_var;
-  7          dbms_output.put_line(area_var);
-  8          insert into assignment values(radius_var,area_var);
-  9      end loop;
- 10  end;
- 11  /
-        area_var:=pi*radius_var*radius_var;
-                  *
-ERROR at line 6:
-ORA-06550: line 6, column 19:
-PLS-00201: identifier 'PI' must be declared
-ORA-06550: line 6, column 9:
-PL/SQL: Statement ignored
-
-
-SQL> declare
-  2      radius_var number;
-  3      area_var number;
   4      pi number:=3.14;
   5  begin
-  6      for radius_var in 5..8 loop
+  6      for radius_var in 5..9 loop
   7          area_var:=pi*radius_var*radius_var;
   8          dbms_output.put_line(area_var);
   9          insert into assignment values(radius_var,area_var);
@@ -44,6 +24,7 @@ SQL> declare
 113.04
 153.86
 200.96
+254.34
 
 PL/SQL procedure successfully completed.
 
@@ -55,8 +36,40 @@ SQL> select * from assignment;
          6     113.04
          7     153.86
          8     200.96
+         9     254.34
 
 SQL>
+
+
+--in sql developer
+--first create table after running it comment it 
+--because it will created only once
+
+
+--create table assignment_3b(radius number,area number);
+
+declare
+      radius_var number;
+      area_var number;
+      pi number:=3.14;
+begin
+      for radius_var in 5..8 loop
+      area_var:=pi*radius_var*radius_var;
+          dbms_output.put_line(area_var);
+          insert into assignment_3b values(radius_var,area_var);
+      end loop;
+end;
+/
+
+output-
+78.5
+113.04
+153.86
+200.96
+
+
+PL/SQL procedure successfully completed.
+
 
 
 
