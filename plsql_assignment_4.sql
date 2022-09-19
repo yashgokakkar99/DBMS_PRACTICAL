faf7568b56543443f1a48487d0d674b06671ae72
Ashutosh Raj Gupta <89141709+AshutoshRajGupta@users.noreply.github.com>
2022-09-19 20:14:32 +0530

    Update plsql_assignment_4.sql

--- plsql_assignment_4.sql
+++ plsql_assignment_4.sql
@@ -42,36 +42,3 @@ SQL> select * from circle_area;
 SQL>
 
 
---in sql developer
---first create table after running it comment it 
---because it will created only once
-
-
---create table assignment_3b(radius number,area number);
-
-declare
-      radius_var number;
-      area_var number;
-      pi number:=3.14;
-begin
-      for radius_var in 5..8 loop
-      area_var:=pi*radius_var*radius_var;
-          dbms_output.put_line(area_var);
-          insert into assignment_3b values(radius_var,area_var);
-      end loop;
-end;
-/
-
-output-
-78.5
-113.04
-153.86
-200.96
-
-
-PL/SQL procedure successfully completed.
-
-
---delete from assignment_3b;
-
-
