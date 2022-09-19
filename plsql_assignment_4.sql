2bdee2f9574b45cb11b278da82d8b4cbd0467ef8
Ashutosh Raj Gupta <89141709+AshutoshRajGupta@users.noreply.github.com>
2022-09-19 14:29:41 +0530

    Update plsql_assignment_4.sql

--- plsql_assignment_4.sql
+++ plsql_assignment_4.sql
@@ -11,13 +11,17 @@ radius_var number;
 area_var number;
 pi number:= 3.14;
 begin
+dbms_output.put_line('Area of circle from radius 5 to 9'); 
 for radius_var in 5..9 loop
 area_var:=pi*radius_var*radius_var;
+
 dbms_output.put_line(area_var);
 insert into circle_area values(radius_var,area_var);
 end loop;
+
 end;
 /
+
 
 
 select * from circle_area;
